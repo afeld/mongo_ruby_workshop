deecec6f4194217dbcb5f4a7a69a135fa497f929
Aidan Feldman <aidan.feldman@gmail.com>
2013-03-27 08:11:12 -0400

    refactor data seeding script to be a class

--- load_data.rb
+++ load_data.rb
@@ -1,42 +1,61 @@
 require 'json'
-# docs: http://rubydoc.info/gems/mongo/1.8.4/frames
 require 'mongo'
 
-include Mongo
+class MongoWrapper
+  include Mongo
 
+  def connection
+    @connection ||= MongoClient.new('localhost', 27017)
+  end
 
-# connect to the DB
-mongo = MongoClient.new('localhost', 27017)
-db = mongo['mongo_ruby_demo']
+  def db
+    @db ||= self.connection['mongo_ruby_demo']
+  end
 
-# remove all non-built-in collections
-db.collections.each do |coll|
-  coll.remove unless coll.name.start_with?('system.')
+  # remove all non-built-in collections
+  def clear
+    self.db.collections.each do |coll|
+      coll.remove unless coll.name.start_with?('system.')
+    end
+  end
+
+  def checkin_seed_data
+    # read in the dummy data
+    json_str = File.read('./data/checkins.json')
+    json = JSON.parse(json_str)
+    json['response']['recent']
+  end
+
+  def seed
+    users_coll = self.db['users']
+    venues_coll = self.db['venues']
+    checkins_coll = self.db['checkins']
+
+    checkins = self.checkin_seed_data
+
+    checkins.each do |checkin|
+      # we don't want to keep the data embedded
+      user = checkin.delete 'user'
+      venue = checkin.delete 'venue'
+
+      user_id = users_coll.insert user
+      venue_id = venues_coll.insert venue
+
+      # reference those newly-created documents
+      checkin['user_id'] = user_id
+      checkin['venue_id'] = venue_id
+
+      checkins_coll.insert checkin
+    end
+
+    # just a sanity check
+    raise "not all checkins loaded" unless checkins.size == checkins_coll.count
+  end
 end
 
-users_coll = db['users']
-venues_coll = db['venues']
-checkins_coll = db['checkins']
-
-
-# read in the dummy data
-checkins = JSON.parse(File.read('./data/checkins.json'))['response']['recent']
-
-# seed the DB
-checkins.each do |checkin|
-  # we don't want to keep the data embedded
-  user = checkin.delete 'user'
-  venue = checkin.delete 'venue'
-
-  user_id = users_coll.insert user
-  venue_id = venues_coll.insert venue
-
-  # reference those newly-created documents
-  checkin['user_id'] = user_id
-  checkin['venue_id'] = venue_id
-
-  checkins_coll.insert checkin
+if __FILE__ == $0
+  # file being run directly
+  wrapper = MongoWrapper.new
+  wrapper.clear
+  wrapper.seed
 end
-
-# just a sanity check
-raise "not all checkins loaded" unless checkins.size == checkins_coll.count
