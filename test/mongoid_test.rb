cb965eee27016f24d681d4d0abce2e0652a86ffe
Aidan Feldman <aidan.feldman@gmail.com>
2013-03-27 12:43:09 -0400

    add test around associations in Mongoid

--- test/mongoid_test.rb
+++ test/mongoid_test.rb
@@ -9,14 +9,18 @@ Mongoid.load! File.join(File.dirname(__FILE__), 'mongoid.yml'), :development
 
 class Checkin
   include Mongoid::Document
+  belongs_to :user
+  belongs_to :venue
 end
 
 class User
   include Mongoid::Document
+  has_many :checkins
 end
 
 class Venue
   include Mongoid::Document
+  has_many :checkins
 end
 
 
@@ -36,7 +40,7 @@ describe Mongoid do
       Venue.all.size.must_equal 10
     end
 
-    it "should insert a new checkin" do
+    it "should insert a new checkin with foreign keys" do
       user_id = User.first.id
       # simplified checkin
       checkin = Checkin.new(
@@ -53,6 +57,29 @@ describe Mongoid do
 
       Checkin.all.size.must_equal 11
       Checkin.where(user_id: user_id).size.must_equal 2
+    end
+
+    it "should insert a new checkin and assign relations" do
+      user = User.first
+      # simplified checkin
+      checkin = Checkin.new(
+        createdAt: Time.now.to_i,
+        type: 'checkin',
+        shout: "just testing"
+      )
+
+      checkin.user = user
+      # pick an arbitrary venue
+      checkin.venue = Venue.first
+
+      checkin.save!
+
+      Checkin.all.size.must_equal 11
+      Checkin.where(user_id: user.id).size.must_equal 2
+
+      checkins = user.checkins
+      checkins.size.must_equal 2
+      checkins.last.must_equal checkin
     end
   end
 end
